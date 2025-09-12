5366f8816312eb65d7a3646163eeefec9fdb0e7a
KevinSotoMiranda <levinsoto5@gmail.com>
2025-09-12 14:32:43 -0600

    Add enemy prefabs for testing

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,30 +8,39 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private List<EnemyType> enemies;
     [SerializeField]
-    private float spawnInterval;
+    private float spawnInterval = 4f;
     [SerializeField]
     private float creditGainRate = 1f;
     [SerializeField]
     private float difficultyScale = 1.05f;
     [SerializeField]
     private int credits = 5;
+    [SerializeField]
+    private Transform playerLocation;
+    [SerializeField]
+    private float spawnRadius = 10f;
     private float spawnTimer;
     private float waveTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        this.spawnTimer = this.spawnInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.credits += (int)Math.Ceiling(this.creditGainRate * Time.deltaTime);
+        this.creditGainRate *= Mathf.Pow(difficultyScale, Time.deltaTime);
 
-        GenerateWave();
+        this.spawnTimer -= Time.deltaTime;
+        if (this.spawnTimer <= 0f)
+        {
+            GenerateWave();
 
-        this.creditGainRate *= difficultyScale * Time.deltaTime;
+            this.spawnTimer = this.spawnInterval;
+        }
     }
 
     private void GenerateWave()
@@ -61,7 +71,34 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy(EnemyType enemy)
     {
+        Vector3 location = enemy.isFlying ? GetAirLocation() : GetGroundLocation();
 
+        Instantiate(enemy.prefab, location, Quaternion.identity);
+    }
+
+    private Vector3 GetGroundLocation()
+    {
+        Vector2 locationOffset = UnityEngine.Random.insideUnitCircle * this.spawnRadius;
+        Vector3 spawnLocation = this.playerLocation.position + new Vector3(locationOffset.x, 0, locationOffset.y);
+
+        float heightOffset = 5f;
+        float raycastLength = 40f;
+        
+        // Shoot a raycast down to determine the grounds Y position
+        if (Physics.Raycast(spawnLocation + Vector3.up * heightOffset, Vector3.down, out RaycastHit hitInfo, raycastLength))
+        {
+            spawnLocation.y = hitInfo.point.y;
+        }
+
+        return spawnLocation;
+    }
+
+    private Vector3 GetAirLocation()
+    {
+        Vector3 locationOffset = UnityEngine.Random.onUnitSphere  * this.spawnRadius;
+        locationOffset.y = Math.Abs(locationOffset.y);
+
+        return this.playerLocation.position + locationOffset;
     }
 }
 
@@ -71,4 +108,5 @@ public class EnemyType
     public string name;
     public GameObject prefab;
     public int cost;
+    public bool isFlying;
 }
