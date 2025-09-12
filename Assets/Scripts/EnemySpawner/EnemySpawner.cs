4c60aab07fe38ddf5b62a7d347aa23610493a805
KevinSotoMiranda <levinsoto5@gmail.com>
2025-09-12 12:50:14 -0600

    Add implementation to spawner

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -9,11 +9,13 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private float spawnInterval;
     [SerializeField]
-    private float creditGainRate;
+    private float creditGainRate = 1f;
     [SerializeField]
-    private float difficultyScale;
-    private int enemyCredits;
+    private float difficultyScale = 1.05f;
+    [SerializeField]
+    private int credits = 5;
     private float spawnTimer;
+    private float waveTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,15 +26,40 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        this.credits += (int)Math.Ceiling(this.creditGainRate * Time.deltaTime);
 
+        GenerateWave();
+
+        this.creditGainRate *= difficultyScale * Time.deltaTime;
     }
 
     private void GenerateWave()
-    { 
-        
+    {
+        List<EnemyType> generatedEnemies = new List<EnemyType>();
+
+        while (this.credits > 0)
+        {
+            EnemyType randomEnemy = enemies[UnityEngine.Random.Range(0, enemies.Count)];
+
+            if (this.credits >= randomEnemy.cost)
+            {
+                this.credits -= randomEnemy.cost;
+                generatedEnemies.Add(randomEnemy);
+            }
+
+            else
+            {
+                continue;
+            }
+        }
+
+        foreach (EnemyType enemy in generatedEnemies)
+        {
+            SpawnEnemy(enemy);
+        }
     }
 
-    private void SpawnEnemy()
+    private void SpawnEnemy(EnemyType enemy)
     {
 
     }
