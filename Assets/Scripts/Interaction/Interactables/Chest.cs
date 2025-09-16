9c3b803809c67a78e488861bf878fed43416be40
shotpb <122491873+shotpb@users.noreply.github.com>
2025-09-16 14:22:12 -0600

    Added gold checking and loot table

--- Assets/Scripts/Interaction/Interactables/Chest.cs
+++ Assets/Scripts/Interaction/Interactables/Chest.cs
@@ -9,7 +9,7 @@ public class Chest : MonoBehaviour, IInteractable
     [SerializeField] private int price = 10;
     [SerializeField] private bool singleActivation = true;
     [SerializeField] private AudioSource audioData;
-    [SerializeField] private GameObject item;
+    [SerializeField] private LootTable lootTable;
     public string InteractionPrompt => "[E] - " + price + "G";
     public bool SingleActivation => singleActivation;
     private bool canInteract = true;
@@ -21,27 +21,30 @@ public class Chest : MonoBehaviour, IInteractable
         {
             // Check if the interactor has enough money
 
-            // if (interactor.GetComponent<PlayerMoney>() >= price)
-            // {
-            //`     interactor.GetComponent<PlayerMoney>() -= price;
-            // };
-            // Disable further interactions
-
-            // Play sounds
-            audioData = GetComponent<AudioSource>();
-            audioData.Play(0);
-            // Spawn items
-            if (item != null)
+            if (interactor.GetComponent<PlayerGold>().SpendGold(price))
             {
-                Instantiate(item, transform.position + Vector3.up, Quaternion.identity);
+                Debug.Log("U have money.");
+                // Play sounds
+                audioData = GetComponent<AudioSource>();
+                audioData.Play(0);
+                // Spawn items
+                if (lootTable != null)
+                {
+                    //Instantiate(item, transform.position + Vector3.up, Quaternion.identity);
+                    lootTable.DoDrop();
+                }
+                else
+                {
+                    // Spawn random item perhaps
+                    //Instantiate(item, transform.position + Vector3.up, Quaternion.identity);
+                }
+                Destroy(gameObject, 1f); // Add a Delay to allow sound to play and block subsequent interactions
+                return true;
             }
             else
             {
-                // Spawn random item perhaps
-                //Instantiate(item, transform.position + Vector3.up, Quaternion.identity);
+                Debug.Log("U broke.");
             }
-            Destroy(gameObject, 1f); // Add a Delay to allow sound to play and block subsequent interactions
-            return true;
         }
 
         if (SingleActivation)
