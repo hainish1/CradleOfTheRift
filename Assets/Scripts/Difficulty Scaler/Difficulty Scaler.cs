b5282787f4fbebb4831ef16e7405c887f38c1757
Mpease0221 <mpease0221@gmail.com>
2025-09-25 15:28:22 -0600

    Outlined Difficulty Scaler

---
+++ Assets/Scripts/Difficulty Scaler/Difficulty Scaler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DifficultyScaler : MonoBehaviour
+{
+    public string[] Difficulties;
+    private float DifficultyScale;
+    public float timePerDifficulty;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetCurrentDifficultyName()
+    {
+        
+    }
+
+    public float GetDifficultyScale()
+    {
+        
+    }
+}
