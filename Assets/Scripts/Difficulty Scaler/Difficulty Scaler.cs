f1aa0e11a4b935f0539ee647b721535e8a14fbc8
Mpease0221 <mpease0221@gmail.com>
2025-09-26 02:19:05 -0600

    Coded Difficulty Scaler

--- Assets/Scripts/Difficulty Scaler/Difficulty Scaler.cs
+++ Assets/Scripts/Difficulty Scaler/Difficulty Scaler.cs
@@ -2,30 +2,49 @@ using UnityEngine;
 
 public class DifficultyScaler : MonoBehaviour
 {
-    public string[] Difficulties;
-    private float DifficultyScale;
-    public float timePerDifficulty;
+    [Header("Difficulty Scaler")]
+    // I did this so you can have customizable difficulty names.
+    // This should make it easy to update the UI!
+    public string[] difficulties;
+    // How many seconds it takes to transition to the next difficulty.
+    public float timePerDifficulty = 5;
+    // How many times the scale should be updated over the previously mentioned interval.
+    // This doesn't really matter and I guess will only ever be used to make the UI possibly smoother.
+    public int updatesPerDifficulty = 20;
+    // The actual scale.
+    // This can be used for boosting loot
+    // Or boosting enemy spawn rates!
+    private float difficultyScale = 1f;
+    private float difficultyPerUpdate;
+    private float updateRate;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        difficultyPerUpdate = 1f / updatesPerDifficulty;
+        updateRate = timePerDifficulty / updatesPerDifficulty;
+        InvokeRepeating(nameof(IncreaseDifficultyScale), updateRate, updateRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void IncreaseDifficultyScale()
     {
-        
+        difficultyScale += difficultyPerUpdate;
     }
 
     public string GetCurrentDifficultyName()
     {
-        
+        int difficultyIndex = (int) (difficultyScale - 1);
+        // If we are over max difficulty, clamp to the max difficulty.
+        if (difficultyIndex > difficulties.Length - 1)
+        {
+            difficultyIndex = difficulties.Length - 1;
+        }
+        return difficulties[difficultyIndex];
     }
 
     public float GetDifficultyScale()
     {
-        
+        return difficultyScale;
     }
 }
