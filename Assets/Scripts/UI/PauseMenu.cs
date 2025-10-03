4a58d6dc12f3e2dd7a3f22f9b40f9c30568686b5
Jhildt97 <jhildt97@gmail.com>
2025-10-02 23:45:57 -0600

    Bare bones Start Menu Up and running
    To use as is, when building the scenes, set mine as the first scene, and the gym as the next scene. When Start Game is selected, it will open the gym scene.
    
    If you want to change the scene to lead when Start Game button is pressed, change the name in the following line:
    SceneManager.LoadScene("Gym");
    
    Where "Gym" is the scene to load

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,24 +1,73 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
 public class PauseMenu : MonoBehaviour
 {
     private UIDocument document;
-    private Button button;
+    private Button startButton;
+    private Button settingsButton; // Future setup
+    private Button quitButton;
+
+    [SerializeField] private UIDocument settingsMenu; // Future setup
+
 
     private void Awake()
     {
         document = GetComponent<UIDocument>();
-        button = document.rootVisualElement.Q("ButtonStartGame") as Button;
-        button.RegisterCallback<ClickEvent>(OnStartGameClick);
+
+        startButton = document.rootVisualElement.Q("ButtonStartGame") as Button;
+        settingsButton = document.rootVisualElement.Q("ButtonSettings") as Button;
+        quitButton = document.rootVisualElement.Q("ButtonQuitGame") as Button;
+
+
+
+        if (startButton != null)
+            startButton.RegisterCallback<ClickEvent>(OnStartGameClick);
+
+        if (settingsButton != null)
+            settingsButton.RegisterCallback<ClickEvent>(OnSettingsClick);
+
+        if (quitButton != null)
+            quitButton.RegisterCallback<ClickEvent>(OnQuitGameClick);
     }
 
     private void OnDisable()
     {
-        button.UnregisterCallback<ClickEvent>(OnStartGameClick);
+        if (startButton != null)
+            startButton.UnregisterCallback<ClickEvent>(OnStartGameClick);
+
+        if (settingsButton != null)
+            settingsButton.UnregisterCallback<ClickEvent>(OnSettingsClick);
+
+        if (quitButton != null)
+            quitButton.UnregisterCallback<ClickEvent>(OnQuitGameClick);
+
     }
 
     private void OnStartGameClick(ClickEvent evt)
     {
-        Debug.Log("You Pressed the Start Button");
+        // Debug.Log("You Pressed the Start Button");
+        SceneManager.LoadScene("Gym");// Change name to game
+
     }
+
+    private void OnQuitGameClick(ClickEvent evt)
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    
+    private void OnSettingsClick(ClickEvent evt)
+    {
+        // Debug.Log("Opening settings menu...");
+        // if (settingsMenu != null)
+        //     settingsMenu.gameObject.SetActive(true);
+    }
+
 }
