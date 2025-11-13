19b329dc3149ba4d9b38d32195ed684b81e6aa2a
shotpb <122491873+shotpb@users.noreply.github.com>
2025-11-13 01:29:57 -0700

    Basic setup for the wisp boss

---
+++ Assets/Scripts/Enemy/RevenantBoss/RevenantBossRange.cs
@@ -0,0 +1,143 @@
+using UnityEngine;
+
+
+/// <summary>
+/// Class - Represents a Range Enemy, inherits from Base Enemy class 
+/// ,also defines functionality of its own
+/// </summary>
+public class RevenantBossRange : Enemy
+{
+    public float projectileDamage = 1;
+
+    [Header("Hover and movement")]
+    public float hoverHeight = 2f; // height above ground
+    public float hoverBobAmplitude = 0.25f; // up n down
+    public float hoverBobSpeed = 2f;
+    public float chaseSpeed = 3.5f;
+    public float stopDistance = 7f; // how far away from player should it stop 
+    public float attackRange = 12f;
+    public float turnSpeedWhileAiming = 12f;
+    public float agentAngularSpeed = 720f;
+    public float agentAcceleration = 100f;
+    
+    [Space]
+
+    [Header("Shooting")]
+    public float projectileSpeed = 50f;
+    public float fireCooldown = .6f;
+    public Transform firePoint; // where bullet come from
+    public EnemyProjectile projectilePrefab;
+    public LayerMask projectileMask = ~0;
+    public float spawnOffset = 0.1f; // a little away fro fire point, safety
+
+    [Space]
+
+    [Header("Reccovery")]
+    [Tooltip("After all orbs are finished, how much time to start again, basically reload time")]
+    public float recoveryTime = 0.4f;
+
+    IdleState_Range idle;
+    ChaseState_Range chase;
+    AttackState_Range attack;
+    RecoveryState_Range recovery;
+
+    float bobPhase;
+    public EnemyRangeOrbitVisuals orbitVisuals;
+
+
+    public override void Start()
+    {
+        base.Start();
+
+        orbitVisuals = GetComponent<EnemyRangeOrbitVisuals>();
+        if (agent != null)
+        {
+            agent.speed = chaseSpeed;
+            agent.angularSpeed = agentAngularSpeed;
+            agent.acceleration = agentAcceleration;
+            agent.autoBraking = true;
+            agent.stoppingDistance = stopDistance * 0.8f;
+        }
+
+        idle = new IdleState_Range(this, stateMachine);
+        chase = new ChaseState_Range(this, stateMachine);
+        attack = new AttackState_Range(this, stateMachine);
+        recovery = new RecoveryState_Range(this, stateMachine);
+
+        stateMachine.Initialize(idle); // enter idle first
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        UpdateHover();
+    }
+
+    /// <summary>
+    /// Apply Hovering Visuals to the Enemy
+    /// </summary>
+    void UpdateHover()
+    {
+        if (agent == null) return;
+
+        bobPhase += Time.deltaTime * hoverBobSpeed;
+        agent.baseOffset = hoverHeight + Mathf.Sin(bobPhase) * hoverBobAmplitude; // usign sin formula for bobbing
+    }
+
+    // HELPERS
+
+    public EnemyState GetIdle() => idle;
+    public EnemyState GetChase() => chase;
+    public EnemyState GetAttack() => attack;
+    public EnemyState GetRecovery() => recovery;
+
+    /// <summary>
+    /// Used to fire one projectile in direction of te target
+    /// </summary>
+    public void FireOnce()
+    {
+        if (!firePoint || !projectilePrefab) return;
+
+        Vector3 direction = (target ? (target.position + Vector3.up * .5f) - firePoint.position : transform.forward).normalized;
+
+        Vector3 spawnPoint = firePoint.position + direction * spawnOffset;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        EnemyProjectile projectile = Instantiate(projectilePrefab, spawnPoint, rotation);
+        projectile.Init(direction * projectileSpeed, projectileMask, this.projectileDamage);
+
+        if (orbitVisuals != null)
+        {
+            int orbIndex = orbitVisuals.GetNextVisibleOrbIndex();
+            if (orbIndex >= 0)
+            {
+                orbitVisuals.HideOrb(orbIndex);
+            }
+            else
+            {
+                // no orbs left,maybe i can go to recovery
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used to initialize damage done by this Range enemy when it is initialized. New Damage value can be initialized using this.
+    /// </summary>
+    /// <param name="newDamage"></param>
+    public void InitializeDamage(float newDamage)
+    {
+        // this.projectileDamage = Mathf.CeilToInt(newDamage);
+        this.projectileDamage = newDamage;
+        Debug.Log("Projectile Damage: " + this.projectileDamage);
+    }
+
+    /// <summary>
+    /// Get Base Damage of this Range Enemy
+    /// </summary>
+    /// <returns></returns>
+    public float GetBaseDamage() => projectileDamage;
+
+
+
+}
