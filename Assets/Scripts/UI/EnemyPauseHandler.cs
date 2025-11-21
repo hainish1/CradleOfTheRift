a14253598ed76e2b170486a7f12013596f968eac
Jhildt97 <jhildt97@gmail.com>
2025-11-21 16:25:44 -0700

    Comment out code
    Not currently working

--- Assets/Scripts/UI/EnemyPauseHandler.cs
+++ Assets/Scripts/UI/EnemyPauseHandler.cs
@@ -1,16 +1,59 @@
-using UnityEngine;
+// using UnityEngine;
+// using UnityEngine.AI;
+// using System.Collections.Generic;
+// using System.Linq;
 
-public class EnemyPauseHandler : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+// public class EnemyPauseManager : MonoBehaviour
+// {
+//     [SerializeField] private LayerMask enemyLayer; // Assign the Enemy layer here in Inspector
+//     private List<IPausable> pausableEnemies = new List<IPausable>();
+
+//     private class EnemyData
+//     {
+//         public NavMeshAgent agent;
+//         public Rigidbody rb;
+//         public Animator anim;
+//         public bool wasStopped;
+//         public float animSpeed;
+//     }
+
+
+
+//     private EnemyData[] allEnemies;
+// private void Update()
+// {
+//     if (PauseManager.GameIsPaused)
+//         PauseAll();
+//     else
+//         ResumeAll();
+// }
+
+// private void PauseAll()
+// {
+//     RebuildPausableList();
+//     foreach (var enemy in pausableEnemies)
+//         enemy.Pause();
+// }
+
+// private void ResumeAll()
+// {
+//     RebuildPausableList();
+//     foreach (var enemy in pausableEnemies)
+//         enemy.Resume();
+// }
+
+// private void RebuildPausableList()
+// {
+//     pausableEnemies.Clear();
+//     IPausable[] enemies = GameObject.FindObjectsByType<MonoBehaviour>(
+//         FindObjectsInactive.Include,// include inactive objects
+//         FindObjectsSortMode.None// don’t sort
+//     )
+//     .OfType<IPausable>()
+//     .ToArray();
+//     foreach (var enemy in enemies)
+//         pausableEnemies.Add(enemy);
+// }
+
+// }
