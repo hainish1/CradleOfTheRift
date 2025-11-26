1dc4a41ba253151ba968a285708abc12e2be7505
Mpease0221 <mpease0221@gmail.com>
2025-11-26 01:12:35 -0700

    Added basic music

---
+++ Capstone-TeamCapybara_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_SONG_PERCUSSIANORCHESTRA = 920721062U;
+        static const AkUniqueID PLAY_WATERCREEK = 1729314049U;
+    } // namespace EVENTS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
