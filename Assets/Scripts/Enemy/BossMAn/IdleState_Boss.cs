7148f818a67a50ad34ed402e062255ae6aec54a5
hainish1 <156365204+hainish1@users.noreply.github.com>
2025-11-30 19:31:35 -0700

    value change

--- Assets/Scripts/Enemy/BossMAn/IdleState_Boss.cs
+++ Assets/Scripts/Enemy/BossMAn/IdleState_Boss.cs
@@ -29,23 +29,23 @@ public class IdleState_Boss : EnemyState
         if(idleTimer >= idleDuration)
         {
             // stateMachine.ChangeState(boss.GetBombState());
-            stateMachine.ChangeState(boss.GetExploisionState());
+            // stateMachine.ChangeState(boss.GetExploisionState());
             // stateMachine.ChangeState(boss.GetLeapAttackState());
 
             // alrgith now here I am keeping it pretty random, which attack is chosen, but we can like mess with it if we want
-            // float random = Random.value;
-            // if (random < 0.5f)
-            // {
-            //     stateMachine.ChangeState(boss.GetBombState());
-            // }
-            // else if (random < 0.8f && !boss.IsPlayerTooFar() && !boss.IsPlayerTooClose()) // ensure player is not too far or too close to leap
-            // {
-            //     stateMachine.ChangeState(boss.GetLeapAttackState());
-            // }
-            // else
-            // {
-            //     stateMachine.ChangeState(boss.GetExploisionState());
-            // }
+            float random = Random.value;
+            if (random < 0.5f)
+            {
+                stateMachine.ChangeState(boss.GetBombState());
+            }
+            else if (random < 0.8f && !boss.IsPlayerTooFar() && !boss.IsPlayerTooClose()) // ensure player is not too far or too close to leap
+            {
+                stateMachine.ChangeState(boss.GetLeapAttackState());
+            }
+            else
+            {
+                stateMachine.ChangeState(boss.GetExploisionState());
+            }
             return;
         }
     }
