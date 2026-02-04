a36b51348f358260f3912b02909e4bd1ee5673d7
KevinSotoMiranda <levinsoto5@gmail.com>
2026-02-04 08:25:28 -0700

    Add chest marker

--- Assets/Scripts/UI/Compass/CompassMarker.cs
+++ Assets/Scripts/UI/Compass/CompassMarker.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MarkerType { Cave, Extraction }
+public enum MarkerType { Cave, Extraction, Chest }
 
 public class CompassMarker : MonoBehaviour
 {
